5e991cb380f2126c23931d62401b20134daad433
silverwind <me@silverwind.io>
2017-07-03 18:06:15 +0200

    switch to execa - fixes #2

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 "use strict";
 
-// travis VMs don't have IPs on their interfaces
-// https://docs.travis-ci.com/user/ci-environment/#Networking
-if (process.env.CI) return;
-
 const assert = require("assert");
 const net = require("net");
 const defaultGateway = require(".");
 
-defaultGateway.v4().then(result => {
-  assert(net.isIPv4(result.gateway));
+Promise.all([
+  defaultGateway.v4(),
+  defaultGateway.v6(),
+]).then(results => {
+  assert(net.isIPv4(results[0].gateway));
+  assert(net.isIPv6(results[1].gateway));
 }).catch(err => {
   console.error(err.stack);
   process.exit(1);
