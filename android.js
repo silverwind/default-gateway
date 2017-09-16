1c9c39bd288164e1300e6201465aa26737a94efd
silverwind <me@silverwind.io>
2017-09-16 13:21:22 +0200

    Add untested Android support - #3

---
+++ android.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const net = require("net");
+const execa = require("execa");
+
+const args = {
+  v4: ["-4", "r"],
+  v6: ["-6", "r"],
+};
+
+const get = family => {
+  return execa.stdout("ip", args[family]).then(stdout => {
+    let result;
+
+    (stdout || "").trim().split("\n").some(line => {
+      const [_, gateway, iface] = /default via (.+?) dev (.+?)( |$)/.exec(line) || [];
+      if (gateway && net.isIP(gateway)) {
+        result = {gateway: gateway, interface: (iface ? iface : null)};
+        return true;
+      }
+    });
+
+    if (!result) {
+      throw new Error("Unable to determine default gateway");
+    }
+
+    return result;
+  });
+};
+
+module.exports.v4 = () => get("v4");
+module.exports.v6 = () => get("v6");
