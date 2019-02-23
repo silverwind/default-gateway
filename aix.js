a580cf303ae523ffe71f8a1a48b281f19fc0fa7b
Jesse Gorzinski <jgorzins@us.ibm.com>
2019-02-23 13:37:46 -0600

    Fix style errors

--- aix.js
+++ aix.js
@@ -4,16 +4,16 @@ const execa = require("execa");
 
 const sql = "select NEXT_HOP, LOCAL_BINDING_INTERFACE from QSYS2.NETSTAT_ROUTE_INFO where ROUTE_TYPE='DFTROUTE' and NEXT_HOP!='*DIRECT' and CONNECTION_TYPE=?";
 
-const checkVariant = () => {if(require("os").type() != "OS400") throw new Error("Unsupported AIX variant"); }
+const checkVariant = () => {if (require("os").type() !== "OS400") throw new Error("Unsupported AIX variant"); };
 
 const parse = stdout => {
-  let result;
+  const result;
   try {
-      let resultObj = JSON.parse(stdout);
-      const gateway = resultObj.records[0].NEXT_HOP;
-      const iface = +resultObj.records[0].LOCAL_BINDING_INTERFACE;
-      result = {gateway, iface};
-  } catch {} 
+    let resultObj = JSON.parse(stdout);
+    const gateway = resultObj.records[0].NEXT_HOP;
+    const iface = +resultObj.records[0].LOCAL_BINDING_INTERFACE;
+    result = {gateway, iface};
+  } catch {}
   if (!result) {
     throw new Error("Unable to determine default gateway");
   }
