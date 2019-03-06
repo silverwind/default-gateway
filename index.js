f6dbec268e91b71d351ed04c6a283de34f75806b
silverwind <me@silverwind.io>
2019-03-06 20:35:34 +0100

    tweaks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const platform = require("os").platform();
+const os = require("os");
+const platform = os.platform();
 
 if ([
   "android",
@@ -12,19 +13,23 @@ if ([
   "win32",
   "aix",
 ].indexOf(platform) !== -1) {
-  let platformModule = `./${platform}`;
+  let file;
   if (platform === "aix") {
-    platformModule = (require("os").type() === "OS400") ? "./ibmi.js" : "./sunos.js";
+    // AIX `netstat` output is compatible with Solaris
+    file = `${os.type() === "OS400" ? "ibmi" : "sunos"}.js`;
+  } else {
+    file = `${platform}.js`;
   }
-  const families = require(platformModule);
-  module.exports.v4 = () => families.v4();
-  module.exports.v6 = () => families.v6();
-  module.exports.v4.sync = () => families.v4.sync();
-  module.exports.v6.sync = () => families.v6.sync();
+
+  const m = require(`./${file}`);
+  module.exports.v4 = () => m.v4();
+  module.exports.v6 = () => m.v6();
+  module.exports.v4.sync = () => m.v4.sync();
+  module.exports.v6.sync = () => m.v6.sync();
 } else {
-  const noop = () => { throw new Error(`Unsupported Platform: ${platform}`); };
-  module.exports.v4 = noop;
-  module.exports.v6 = noop;
-  module.exports.v4.sync = noop;
-  module.exports.v6.sync = noop;
+  const unsupported = () => { throw new Error(`Unsupported Platform: ${platform}`); };
+  module.exports.v4 = unsupported;
+  module.exports.v6 = unsupported;
+  module.exports.v4.sync = unsupported;
+  module.exports.v6.sync = unsupported;
 }
