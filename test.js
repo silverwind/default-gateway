13b2fb4eb6d74fcd0900a763a6e44cbe1b0b5291
silverwind <me@silverwind.io>
2019-06-16 13:41:26 +0200

    test for interface property

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// IPv6 tests are skipped because Travis VMs do not support IPv6
-
 const assert = require("assert");
 const net = require("net");
 const defaultGateway = require(".");
@@ -13,19 +11,25 @@ const exit = err => {
 
 const main = async () => {
   const async4 = await defaultGateway.v4();
-  assert(net.isIPv4((async4).gateway));
+  assert(net.isIPv4(async4.gateway));
+  assert(async4.interface);
 
+  // IPv6 tests are skipped because Travis VMs do not support IPv6
   if (!process.env.CI) {
     const async6 = await defaultGateway.v6();
     assert(net.isIPv6(async6.gateway));
+    assert(async6.interface);
   }
 
   const sync4 = defaultGateway.v4.sync();
   assert(net.isIPv4(sync4.gateway));
+  assert(sync4.interface);
 
+  // IPv6 tests are skipped because Travis VMs do not support IPv6
   if (!process.env.CI) {
     const sync6 = defaultGateway.v6.sync();
     assert(net.isIPv6(sync6.gateway));
+    assert(sync6.interface);
   }
 };
 
