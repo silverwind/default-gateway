67c2ef1e5bff0074584c326de586252491be3293
silverwind <me@silverwind.io>
2020-03-11 23:02:13 +0100

    try ipv6 on actions

--- test.js
+++ test.js
@@ -14,23 +14,17 @@ const main = async () => {
   assert(net.isIPv4(async4.gateway));
   assert(async4.interface);
 
-  // IPv6 tests are skipped because Travis VMs do not support IPv6
-  if (!process.env.CI) {
-    const async6 = await defaultGateway.v6();
-    assert(net.isIPv6(async6.gateway));
-    assert(async6.interface);
-  }
+  const async6 = await defaultGateway.v6();
+  assert(net.isIPv6(async6.gateway));
+  assert(async6.interface);
 
   const sync4 = defaultGateway.v4.sync();
   assert(net.isIPv4(sync4.gateway));
   assert(sync4.interface);
 
-  // IPv6 tests are skipped because Travis VMs do not support IPv6
-  if (!process.env.CI) {
-    const sync6 = defaultGateway.v6.sync();
-    assert(net.isIPv6(sync6.gateway));
-    assert(sync6.interface);
-  }
+  const sync6 = defaultGateway.v6.sync();
+  assert(net.isIPv6(sync6.gateway));
+  assert(sync6.interface);
 };
 
 main().then(exit).catch(exit);
