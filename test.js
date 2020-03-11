e1a447dccd380ce667b80b17d506d77cb2f833be
silverwind <me@silverwind.io>
2020-03-11 23:12:44 +0100

    switch to jest

--- test.js
+++ test.js
@@ -1,30 +1,29 @@
 "use strict";
 
-const assert = require("assert");
-const net = require("net");
-const defaultGateway = require(".");
+const {v4, v6} = require(".");
+const {isIPv4, isIPv6} = require("net");
+const {test, expect} = global;
 
-const exit = err => {
-  if (err) console.error(err);
-  process.exit(err ? 1 : 0);
-};
+test("v4 async", async () => {
+  const result = await v4();
+  expect(isIPv4(result.gateway)).toBe(true);
+  expect(typeof result.interface).toBe("string");
+});
 
-const main = async () => {
-  const async4 = await defaultGateway.v4();
-  assert(net.isIPv4(async4.gateway));
-  assert(async4.interface);
+test("v6 async", async () => {
+  const result = await v6();
+  expect(isIPv6(result.gateway)).toBe(true);
+  expect(typeof result.interface).toBe("string");
+});
 
-  const async6 = await defaultGateway.v6();
-  assert(net.isIPv6(async6.gateway));
-  assert(async6.interface);
+test("v4 sync", () => {
+  const result = v4.sync();
+  expect(isIPv4(result.gateway)).toBe(true);
+  expect(typeof result.interface).toBe("string");
+});
 
-  const sync4 = defaultGateway.v4.sync();
-  assert(net.isIPv4(sync4.gateway));
-  assert(sync4.interface);
-
-  const sync6 = defaultGateway.v6.sync();
-  assert(net.isIPv6(sync6.gateway));
-  assert(sync6.interface);
-};
-
-main().then(exit).catch(exit);
+test("v6 sync", () => {
+  const result = v6.sync();
+  expect(isIPv6(result.gateway)).toBe(true);
+  expect(typeof result.interface).toBe("string");
+});
