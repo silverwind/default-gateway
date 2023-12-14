5d684146e2772a8f0a7e1f78772f4d1be6fd0e6e
silverwind <me@silverwind.io>
2023-12-14 13:33:34 +0100

    update test config

--- vitest.config.js
+++ vitest.config.js
@@ -2,5 +2,7 @@ import {defineConfig} from "vitest/dist/config.js";
 import {backendTest} from "vitest-config-silverwind";
 
 export default defineConfig({
-  test: backendTest,
+  test: {
+    ...backendTest({url: import.meta.url}),
+  },
 });
